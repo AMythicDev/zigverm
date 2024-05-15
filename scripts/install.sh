41764bef65df0df7cad19222cd6b0391fd39ec1e
Arijit Dey <arijit79@protonmail.com>
2024-05-15 22:16:19 +0530

    Fix major bugs with install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,29 +1,29 @@
 #!/bin/sh
 
 if [[ -z $ZIGVM_ROOT_DIR ]]; then
-  ZIGVM_ROOT_DIR=$HOME/.zigvm/
+  ZIGVM_ROOT_DIR=$HOME/.zigvm
 fi
 
 mkdir -p $ZIGVM_ROOT_DIR/{downloads,installs,bin}
 
-OS=${uname -s | awk '{print tolower($0)}'}
-ARCH=${uname -m | awk '{print tolower($0)}'}
+OS=$(uname -s | awk '{print tolower($0)}')
+ARCH=$(uname -m | awk '{print tolower($0)}')
 
 curl -L https://github.com/AMythicDev/zigvm/releases/download/v0.1.0/zigvm-0.1.0-${ARCH}-${OS} > $ZIGVM_ROOT_DIR/bin/zigvm
 chmod 755 $ZIGVM_ROOT_DIR/bin/zigvm 
 
-DEFAULT_SHELL=${getent passwd arijit | awk -F: '{ print($NF) } ' | awk -F/ '{ print ($NF) }'}
+DEFAULT_SHELL=$(getent passwd arijit | awk -F: '{ print($NF) } ' | awk -F/ '{ print ($NF) }')
 
 case DEFAULT_SHELL in
   "bash")
-    echo 'export $PATH=$PATH:'$ZIGVM_ROOT_DIR/bin/ >> $HOME/.bashrc
+    echo 'export PATH=$PATH:'$ZIGVM_ROOT_DIR/bin/ >> $HOME/.bashrc
     ;;
   "zsh")
-    echo 'export $PATH=$PATH:'$ZIGVM_ROOT_DIR/bin/ >> $HOME/.zshrc
+    echo 'export PATH=$PATH:'$ZIGVM_ROOT_DIR/bin/ >> $HOME/.zshrc
     ;;
   *)
     echo "Cannot write to shell rc file. Unknown shell" 1>&2;
 esac
 
-echo 'export $PATH=$PATH:'$ZIGVM_ROOT_DIR/bin/ >> $HOME/.profile
+echo 'export PATH=$PATH:'$ZIGVM_ROOT_DIR/bin/ >> $HOME/.profile
 
