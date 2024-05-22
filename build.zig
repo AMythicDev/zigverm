d61c164a961032ca3362d609eafbf749f620fba1
Arijit Dey <arijit79@protonmail.com>
2024-05-22 18:57:29 +0530

    Create a step to build release binaries automatically

--- build.zig
+++ build.zig
@@ -1,57 +1,38 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
-pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
-    const target = b.standardTargetOptions(.{});
+const targets: []const std.Target.Query = &.{
+    .{ .cpu_arch = .x86_64, .os_tag = .macos },
+    .{ .cpu_arch = .aarch64, .os_tag = .macos },
 
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
+    .{ .cpu_arch = .aarch64, .os_tag = .linux },
+    .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu },
+    .{ .cpu_arch = .x86, .os_tag = .linux, .abi = .gnu },
+
+    .{ .cpu_arch = .x86_64, .os_tag = .windows },
+    .{ .cpu_arch = .x86, .os_tag = .windows },
+};
+
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
+    const default_exe = b.addExecutable(.{
         .name = "zigvm",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-
-    const ostag = target.result.os.tag;
-    if (ostag.isBSD() or ostag.isDarwin() or ostag == std.Target.Os.Tag.linux) {
-        exe.linkLibC();
+    const default_os = target.result.os.tag;
+    if (default_os.isBSD() or default_os.isDarwin() or default_os == std.Target.Os.Tag.linux) {
+        default_exe.linkLibC();
     }
+    b.installArtifact(default_exe);
 
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
-    b.installArtifact(exe);
-
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
-    const run_cmd = b.addRunArtifact(exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
+    const run_cmd = b.addRunArtifact(default_exe);
     run_cmd.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
@@ -60,12 +41,26 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
+
+    const release_step = b.step("make-release", "Create binaries for releasing");
+    for (targets) |t| {
+        const name = try std.mem.concat(b.allocator, u8, &.{ "zigvm-", try t.zigTriple(b.allocator) });
+        const exe = b.addExecutable(.{
+            .name = name,
+            .root_source_file = b.path("src/main.zig"),
+            .target = b.resolveTargetQuery(t),
+            .optimize = .ReleaseSafe,
+        });
+
+        const target_output = b.addInstallArtifact(exe, .{});
+        release_step.dependOn(&target_output.step);
+
+        const ostag = target.result.os.tag;
+        if (ostag.isBSD() or ostag.isDarwin() or ostag == std.Target.Os.Tag.linux) {
+            exe.linkLibC();
+        }
+    }
 }
