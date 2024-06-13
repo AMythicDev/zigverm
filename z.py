ab4d1d0f314fb0d6f67f8d9dc2f7ca367848760f
Arijit Dey <arijit79@protonmail.com>
2024-06-13 17:13:24 +0530

    Make release tarballs with a parent dir in archive

--- z.py
+++ z.py
@@ -3,42 +3,47 @@
 import sys
 import subprocess
 import os
-import shutil
+import zipfile
 
-
-VERSION = "0.1.0"
-
+VERSION = "0.2.0"
 
 def make_release():
     targets = [
-        "aarch64-macos",
-        "x86_64-macos",
-        "aarch64-linux",
-        "x86_64-linux",
-        "x86-linux",
-        "x86_64-windows",
-        "x86-windows",
+        ["aarch64", "macos"],
+        ["x86_64", "macos"],
+        ["aarch64", "linux"],
+        ["x86_64", "linux"],
+        ["x86", "linux"],
+        ["x86_64", "windows"],
+        ["x86", "windows"],
     ]
     if not os.path.exists("releases"):
         os.mkdir("releases")
 
     for target in targets:
         print(f"Building for {target}")
-        target_dir = "zigvm-" + VERSION + "-" + target
+        target_str = f"{target[0]}-{target[1]}"
+        target_dir = "zigvm-" + VERSION + "-" + target_str
         subprocess.run(["zig", "build", "install", "--prefix", "releases/",
                        "--prefix-exe-dir", target_dir, "--release=safe",
-                        f"-Dtarget={target}"])
-        shutil.make_archive("releases/" + target_dir,
-                            "zip", "releases/" + target_dir)
+                        f"-Dtarget={target_str}"])
+
+        with zipfile.ZipFile("releases/"+target_dir+".zip", "w") as z:
+            z.write("releases/"+target_dir, target_dir);
+            if target[1] == "windows":
+                exe_ext = ".exe"
+            else:
+                exe_ext = ""
+
+            z.write("releases/"+target_dir+"/zigvm" +
+                    exe_ext, target_dir+"/zigvm")
+            z.write("releases/"+target_dir+"/zig"+exe_ext, target_dir+"/zig")
 
 
 def main():
     args = sys.argv
 
-    if args[1] == "run":
-        subprocess.run(["zig", "build"])
-        subprocess.run(["zig-out/bin/" + args[2]] + args[3:])
-    elif args[1] == "make-release":
+    if args[1] == "make-release":
         make_release()
 
 
