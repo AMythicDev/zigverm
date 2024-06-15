73f931d410d3bdefa85c3b77c1bb115d11522845
Arijit Dey <arijit79@protonmail.com>
2024-06-15 13:18:05 +0530

    Fix all warnings in automatic installer

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
@@ -13,33 +13,36 @@ if [[ -f $ZIGVM_ROOT_DIR/bin/zigvm ]]; then
   exit 0;
 fi
 
-mkdir -p $ZIGVM_ROOT_DIR/{downloads,installs,bin}
+mkdir -p "$ZIGVM_ROOT_DIR"/{downloads,installs,bin}
 
 OS=$(uname -s | awk '{print tolower($0)}')
 ARCH=$(uname -m | awk '{print tolower($0)}')
 
-curl -L https://github.com/AMythicDev/zigvm/releases/download/v${VERSION}/zigvm-${VERSION}-${ARCH}-${OS}.zip > /tmp/zigvm.zip
+curl -L https://github.com/AMythicDev/zigvm/releases/download/v${VERSION}/zigvm-${VERSION}-"${ARCH}-${OS}".zip > /tmp/zigvm.zip
 unzip /tmp/zigvm.zip -d /tmp/
 
-mv /tmp/zigvm-${VERSION}-${ARCH}-${OS}/zigvm $ZIGVM_ROOT_DIR/bin
-mv /tmp/zigvm-${VERSION}-${ARCH}-${OS}/zig $ZIGVM_ROOT_DIR/bin
+mv /tmp/zigvm-${VERSION}-"${ARCH}-${OS}"/zigvm "$ZIGVM_ROOT_DIR/bin"
+mv /tmp/zigvm-${VERSION}-"${ARCH}-${OS}"/zig "$ZIGVM_ROOT_DIR/bin"
 
 echo "Installing zig stable"
-$ZIGVM_ROOT_DIR/bin/zigvm install stable
+"$ZIGVM_ROOT_DIR"/bin/zigvm install stable
 echo "Setting default version to stable"
-$ZIGVM_ROOT_DIR/bin/zigvm override default stable
+"$ZIGVM_ROOT_DIR"/bin/zigvm override default stable
 
-DEFAULT_SHELL=$(getent passwd $USER | awk -F: '{ print($NF) } ' | awk -F/ '{ print ($NF) }')
+DEFAULT_SHELL=$(getent passwd "$USER" | awk -F: '{ print($NF) } ' | awk -F/ '{ print ($NF) }')
 
 case $DEFAULT_SHELL in
   "bash")
-    echo 'export PATH=$PATH:'$ZIGVM_ROOT_DIR/bin >> $HOME/.bashrc
+    # shellcheck disable=SC2016
+    echo 'export PATH=$PATH:'"$ZIGVM_ROOT_DIR/bin" >> "$HOME/.bashrc"
     ;;
   "zsh")
-    echo 'export PATH=$PATH:'$ZIGVM_ROOT_DIR/bin >> $HOME/.zshrc
+    # shellcheck disable=SC2016
+    echo 'export PATH=$PATH:'"$ZIGVM_ROOT_DIR/bin" >> "$HOME/.zshrc"
     ;;
   "fish")
-    echo 'set -x PATH=$PATH:'$ZIGVM_ROOT_DIR/bin >> ${XDG_CONFIG_HOME:-$HOME/.config}/fish/config.fish
+    # shellcheck disable=SC2016
+    echo 'set -x PATH=$PATH:'"$ZIGVM_ROOT_DIR/bin" >> "${XDG_CONFIG_HOME:-$HOME/.config}/fish/config.fish"
     ;;
   *)
     echo "Cannot write to shell rc file. Unknown shell" 1>&2;
@@ -47,6 +50,7 @@ case $DEFAULT_SHELL in
     ;;
 esac
 
-echo 'export PATH=$PATH:'$ZIGVM_ROOT_DIR/bin/ >> $HOME/.profile
+# shellcheck disable=SC2016
+echo 'export PATH=$PATH:'"$ZIGVM_ROOT_DIR"/bin/ >> "$HOME"/.profile
 
 echo -e "\033[0;33;1mzigvm installed successfully\nPlease restart your terminal for changes to take effect.\033[0m"
