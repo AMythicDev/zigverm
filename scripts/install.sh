3fda9d45d51afd22cc96645b000b15ce00db4360
Arijit Dey <arijit79@protonmail.com>
2024-10-29 02:09:04 +0530

    fix: avoid adding .zigverm/bin to PATH if it already exists

--- scripts/install.sh
+++ scripts/install.sh
@@ -39,28 +39,30 @@ echo "Installing zig stable"
 echo "Setting default version to stable"
 "$ZIGVERM_ROOT_DIR"/bin/zigverm override default stable
 
-DEFAULT_SHELL=$(getent passwd "$USER" | awk -F: '{ print($NF) } ' | awk -F/ '{ print ($NF) }')
+if [[ -x "$(command -v zigverm)" ]]; then
+    DEFAULT_SHELL=$(getent passwd "$USER" | awk -F: '{ print($NF) } ' | awk -F/ '{ print ($NF) }')
 
-case $DEFAULT_SHELL in
-  "bash")
-    # shellcheck disable=SC2016
-    echo 'export PATH=$PATH:'"$ZIGVERM_ROOT_DIR/bin" >> "$HOME/.bashrc"
-    ;;
-  "zsh")
-    # shellcheck disable=SC2016
-    echo 'export PATH=$PATH:'"$ZIGVERM_ROOT_DIR/bin" >> "$HOME/.zshrc"
-    ;;
-  "fish")
-    # shellcheck disable=SC2016
-    echo 'set -x PATH=$PATH:'"$ZIGVERM_ROOT_DIR/bin" >> "${XDG_CONFIG_HOME:-$HOME/.config}/fish/config.fish"
-    ;;
-  *)
-    echo "Cannot write to shell rc file. Unknown shell" 1>&2;
-    echo "You need to manually add {$ZIGVERM_ROOT_DIR}/bin to ensure zigverm can be called from anywhere."
-    ;;
-esac
+    case $DEFAULT_SHELL in
+    "bash")
+        # shellcheck disable=SC2016
+        echo 'export PATH=$PATH:'"$ZIGVERM_ROOT_DIR/bin" >> "$HOME/.bashrc"
+        ;;
+    "zsh")
+        # shellcheck disable=SC2016
+        echo 'export PATH=$PATH:'"$ZIGVERM_ROOT_DIR/bin" >> "$HOME/.zshrc"
+        ;;
+    "fish")
+        # shellcheck disable=SC2016
+        echo 'set -x PATH=$PATH:'"$ZIGVERM_ROOT_DIR/bin" >> "${XDG_CONFIG_HOME:-$HOME/.config}/fish/config.fish"
+        ;;
+    *)
+        echo "Cannot write to shell rc file. Unknown shell" 1>&2;
+        echo "You need to manually add {$ZIGVERM_ROOT_DIR}/bin to ensure zigverm can be called from anywhere."
+        ;;
+    esac
 
-# shellcheck disable=SC2016
-echo 'export PATH=$PATH:'"$ZIGVERM_ROOT_DIR"/bin/ >> "$HOME"/.profile
+    # shellcheck disable=SC2016
+    echo 'export PATH=$PATH:'"$ZIGVERM_ROOT_DIR"/bin/ >> "$HOME"/.profile
+fi
 
 echo -e "\033[0;33;1mzigverm installed successfully\nPlease restart your terminal for changes to take effect.\033[0m"
