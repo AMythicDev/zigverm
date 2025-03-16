107fa3fb98f18337f52a01f7fbbb4d1a2281c2ab
Arijit Dey <arijit79@protonmail.com>
2025-03-16 23:20:50 +0530

    write a flake file for zigverm

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Version Manager for the Zig Programming Language";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+      flake-utils.lib.eachDefaultSystem (system: {
+      packages.zigverm = 
+        with import nixpkgs { inherit system; };
+        stdenv.mkDerivation(finalAttrs: {
+          name = "zigverm";
+          version = "0.6.2";
+
+          src = fetchFromGitHub {
+            owner = "AMythicDev";
+            repo = "zigverm";
+            rev = "v${finalAttrs.version}";
+            hash =  "sha256-RcvDzA4gpyzRxsJQgxOpZT9MAkD9kndov0r6QQyr/DY=";
+          };
+
+          nativeBuildInputs = [
+            zig_0_13.hook
+          ];
+        });
+        defaultPackage = self.packages.${system}.zigverm;
+      }
+  );
+}
