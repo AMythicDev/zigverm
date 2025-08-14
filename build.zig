99bf59b28f5a191dc468558626167181b3d29667
Arijit Dey <arijit79@protonmail.com>
2025-08-15 03:49:50 +0530

    ci: fix build file options

--- build.zig
+++ build.zig
@@ -13,20 +13,20 @@ pub fn build(b: *std.Build) !void {
 
     const strip = if (optimize == std.builtin.OptimizeMode.ReleaseSafe) true else null;
 
-    const zigverm = b.addExecutable(.{
-        .name = "zigverm",
-        .root_source_file = b.path("src/main.zig"),
+    const zigverm = b.addExecutable(
+        .{ .name = "zigverm", .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .strip = strip,
+        }) },
+    );
+    const zig = b.addExecutable(.{ .name = "zig", .root_module = b.createModule(.{
+        .root_source_file = b.path("src/zig/main.zig"),
         .target = target,
         .optimize = optimize,
         .strip = strip,
-    });
-    const zig = b.addExecutable(.{
-        .name = "zig",
-        .root_source_file = b.path("src/zig//main.zig"),
-        .target = target,
-        .optimize = optimize,
-        .strip = strip,
-    });
+    }) });
 
     zigverm.root_module.addImport("common", common);
     // zigverm.root_module.addImport("zip", zip.module("zip"));
@@ -57,25 +57,25 @@ fn addExeRunner(b: *std.Build, zigverm: *Compile, zig: *Compile) void {
 }
 
 fn addTestRunner(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) void {
-    const zigverm_tests = b.addTest(.{
+    const zigverm_tests = b.addTest(.{ .root_module = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
-    });
+    }) });
     const run_zigverm_tests = b.addRunArtifact(zigverm_tests);
 
-    const zig_tests = b.addTest(.{
+    const zig_tests = b.addTest(.{ .root_module = b.createModule(.{
         .root_source_file = b.path("src/zig/main.zig"),
         .target = target,
         .optimize = optimize,
-    });
+    }) });
     const run_zig_tests = b.addRunArtifact(zig_tests);
 
-    const common_tests = b.addTest(.{
-        .root_source_file = b.path("src/common/tests.zig"),
+    const common_tests = b.addTest(.{ .root_module = b.createModule(.{
+        .root_source_file = b.path("src/zig/main.zig"),
         .target = target,
         .optimize = optimize,
-    });
+    }) });
     const default_os = target.result.os.tag;
     if (default_os.isBSD() or default_os.isDarwin() or default_os == std.Target.Os.Tag.linux) {
         common_tests.linkLibC();
